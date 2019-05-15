47b27f8bf78f64160c4e8474d27e85a8393a24c8
Heekyum Kim <shlrur123@gmail.com>
2019-05-15 13:22:19 +0900

    first commit
    is good?

--- src/components/App.js
+++ src/components/App.js
@@ -3,7 +3,7 @@ import React from 'react';
 function App() {
     return (
         <div className="App">
-            <h1>Weekly Business Report</h1>
+            <h1>Weekly Business Report!</h1>
         </div>
     );
 }
